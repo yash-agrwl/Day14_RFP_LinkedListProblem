c94f0fd7a3aef753d97914e0b8082f591412b91b
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-10 11:57:18 +0530

    added empty project file with welcome message

---
+++ LinkedListDataStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListDataStructure
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Linked List Data Structure Problem.\n");
+        }
+    }
+}
