9ac9e29f4f4972444653391d3f3d33b41e6c9b5a
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-10 12:39:23 +0530

    added UC2: Ability to create LinkedList by adding elements at first

--- LinkedListDataStructure/LinkedList.cs
+++ LinkedListDataStructure/LinkedList.cs
@@ -10,5 +10,35 @@ namespace LinkedListDataStructure
     {
 
         private Node<T> Head;
+
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if(Head == null)
+                Head = node;
+            else
+            {
+                node.Next = Head;
+                this.Head = node;
+            }
+            Console.WriteLine("{0} inserted into linked list\n", node.Data);
+        }
+
+        public void Display()
+        {
+            Node<T> temp = this.Head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty\n");
+                return;
+            }
+            Console.WriteLine("The Elements in Linked List are displayed below:");
+            while (temp != null)
+            {
+                Console.Write(temp.Data + " ");
+                temp = temp.Next;
+            }
+            Console.WriteLine();
+        }
     }
 }
--- LinkedListDataStructure/Program.cs
+++ LinkedListDataStructure/Program.cs
@@ -9,6 +9,12 @@ namespace LinkedListDataStructure
             Console.WriteLine("Welcome to the Linked List Data Structure Problem.\n");
 
             LinkedList<int> list = new LinkedList<int>();
+
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+
+            list.Display();
         }
     }
 }
