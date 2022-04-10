ea92ca85bcfe2e6077db922eed46c718aa6d7340
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-10 12:18:03 +0530

    added UC1: created a simple linked list of generic type

---
+++ LinkedListDataStructure/LinkedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDataStructure
+{
+    public class LinkedList<T> 
+    {
+
+        private Node<T> Head;
+    }
+}
--- LinkedListDataStructure/Program.cs
+++ LinkedListDataStructure/Program.cs
@@ -7,6 +7,8 @@ namespace LinkedListDataStructure
         static void Main()
         {
             Console.WriteLine("Welcome to the Linked List Data Structure Problem.\n");
+
+            LinkedList<int> list = new LinkedList<int>();
         }
     }
 }
